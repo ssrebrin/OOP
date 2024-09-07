225ae35788d7a54c2e0d4fb60fc81083939945c0
ssrebtin <s.rebrin@g.nsu.ru>
2024-09-07 23:50:48 +0700

    sample

---
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Task_1_1_1/src/test/java/org/example/MainTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void SampleTest(){
+        assertEquals(3,Main.sum(1,2));
+    }
+}
