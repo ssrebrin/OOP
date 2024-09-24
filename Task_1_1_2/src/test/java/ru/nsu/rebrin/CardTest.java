7116803108a14eeb3ff25a4e1024554714ee326a
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-24 20:18:08 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/rebrin/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/CardTest.java
@@ -1,9 +1,9 @@
 package ru.nsu.rebrin;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class CardTest {
 
--- Task_1_1_2/src/test/java/ru/nsu/rebrin/DealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/DealerTest.java
@@ -1,13 +1,12 @@
 package ru.nsu.rebrin;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class DealerTest {
 
