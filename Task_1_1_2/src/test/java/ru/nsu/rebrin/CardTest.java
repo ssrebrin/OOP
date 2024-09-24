7110f946c95d991e806fd0d7a4ae1974a33b5805
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-24 10:52:44 +0700

    tests

---
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/CardTest.java
@@ -0,0 +1,43 @@
+package ru.nsu.rebrin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class CardTest {
+
+    @Test
+    void testConstructor() {
+        Card card = new Card("Hearts", 11, "A");
+        assertEquals(11, card.meaning);
+        assertEquals("Hearts", card.suit);
+        assertEquals("A", card.value);
+        assertFalse(card.open);
+    }
+
+    @Test
+    void testEquals() {
+        Card card1 = new Card("Hearts", 11, "A");
+        Card card2 = new Card("Hearts", 11, "A");
+        Card card3 = new Card("Diamonds", 10, "10");
+
+        assertTrue(card1.equals(card2));
+        assertFalse(card1.equals(card3));
+        assertFalse(card1.equals(null));
+        assertFalse(card1.equals(new Object()));
+    }
+
+    @Test
+    void testOpen() {
+        Card card = new Card("Hearts", 11, "A");
+        assertFalse(card.open);
+
+        card.open();
+        assertTrue(card.open);
+
+        card.close();
+        assertFalse(card.open);
+    }
+
+}
