7978c0ec1f3a4d8deb9caa71eddd91e7b8f83a5a
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-24 20:22:13 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/rebrin/DealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/DealerTest.java
@@ -12,19 +12,19 @@ class DealerTest {
 
     @Test
     void test1() {
-        User user = new User();
+        Dealer dealer = new Dealer();
         List<Card> deckTest = new ArrayList<>();
 
         deckTest.add(new Card("Ace", 11, "Spades"));
         deckTest.add(new Card("Two", 2, "Diamonds"));
 
-        user.take_card(deckTest, true);
-        user.take_card(deckTest, false);
+        dealer.take_card(deckTest, true);
+        dealer.take_card(deckTest, false);
 
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        user.show_hand(false);
+        dealer.show_hand(false);
 
         String expectedOutput = "Your hand: [Two Diamonds (2), <Close card>]";
         assertEquals(expectedOutput, outContent.toString().trim());
@@ -32,19 +32,19 @@ class DealerTest {
 
     @Test
     void test2() {
-        User user = new User();
+        Dealer dealer = new Dealer();
         List<Card> deckTest = new ArrayList<>();
 
         deckTest.add(new Card("Ace", 11, "Spades"));
         deckTest.add(new Card("Two", 2, "Diamonds"));
 
-        user.take_card(deckTest, false);
-        user.take_card(deckTest, true);
+        dealer.take_card(deckTest, false);
+        dealer.take_card(deckTest, true);
 
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        user.show_hand(false);
+        dealer.show_hand(false);
 
         String expectedOutput = "Your hand: [<Close card>, Ace Spades (11)]";
         assertEquals(expectedOutput, outContent.toString().trim());
@@ -52,18 +52,18 @@ class DealerTest {
 
     @Test
     void test3() {
-        User user = new User();
+        Dealer dealer = new Dealer();
         List<Card> deckTest = new ArrayList<>();
 
         deckTest.add(new Card("Ace", 11, "Spades"));
         deckTest.add(new Card("Ace", 11, "Diamonds"));
 
-        user.take_card(deckTest, true);
-        user.take_card(deckTest, false);
+        dealer.take_card(deckTest, true);
+        dealer.take_card(deckTest, false);
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        user.show_hand(false);
+        dealer.show_hand(false);
 
         String expectedOutput = "Your hand: [Ace Diamonds (11), <Close card>]";
         assertEquals(expectedOutput, outContent.toString().trim());
@@ -71,18 +71,18 @@ class DealerTest {
 
     @Test
     void test4() {
-        User user = new User();
+        Dealer dealer = new Dealer();
         List<Card> deckTest = new ArrayList<>();
 
         deckTest.add(new Card("Ace", 11, "Spades"));
         deckTest.add(new Card("Ace", 11, "Diamonds"));
 
-        user.take_card(deckTest, true);
-        user.take_card(deckTest, true);
+        dealer.take_card(deckTest, true);
+        dealer.take_card(deckTest, true);
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        user.show_hand(true);
+        dealer.show_hand(true);
 
         String expectedOutput = "Your hand: [Ace Diamonds (1), Ace Spades (1)] => 2";
         assertEquals(expectedOutput, outContent.toString().trim());
