0153b8f6d0eaf8f74283411b4dbfc9dba1f2f011
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-24 09:15:43 +0700

    tests

--- Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
@@ -1,16 +1,15 @@
 package ru.nsu.rebrin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-import java.util.Scanner;
 import java.util.ArrayList;
-
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test.
