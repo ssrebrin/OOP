d7245e8aa7653f6f0583bbd84cf7df07300441da
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-25 12:24:44 +0700

    test

--- Task_1_1_2/src/main/java/ru/nsu/rebrin/BlackJack.java
+++ Task_1_1_2/src/main/java/ru/nsu/rebrin/BlackJack.java
@@ -16,80 +16,30 @@ public class BlackJack {
     /**
      * Create the deck and shuffle it.
      *
-     * @return - shaffled deck
+     * @return - shuffled deck
      */
     public List<Card> prepare_deck() {
-        List<Card> deck = new ArrayList<Card>(52);
-        {
-            deck.add(0, new Card("Two", 2, "Spades"));
-            deck.add(1, new Card("Two", 2, "Hearts"));
-            deck.add(2, new Card("Two", 2, "Clubs"));
-            deck.add(3, new Card("Two", 2, "Diamonds"));
-
-            deck.add(4, new Card("Three", 3, "Spades"));
-            deck.add(5, new Card("Three", 3, "Hearts"));
-            deck.add(6, new Card("Three", 3, "Clubs"));
-            deck.add(7, new Card("Three", 3, "Diamonds"));
-
-            deck.add(8, new Card("Four", 4, "Spades"));
-            deck.add(9, new Card("Four", 4, "Hearts"));
-            deck.add(10, new Card("Four", 4, "Clubs"));
-            deck.add(11, new Card("Four", 4, "Diamonds"));
-
-            deck.add(12, new Card("Five", 5, "Spades"));
-            deck.add(13, new Card("Five", 5, "Hearts"));
-            deck.add(14, new Card("Five", 5, "Clubs"));
-            deck.add(15, new Card("Five", 5, "Diamonds"));
-
-            deck.add(16, new Card("Six", 6, "Spades"));
-            deck.add(17, new Card("Six", 6, "Hearts"));
-            deck.add(18, new Card("Six", 6, "Clubs"));
-            deck.add(19, new Card("Six", 6, "Diamonds"));
-
-            deck.add(20, new Card("Seven", 7, "Spades"));
-            deck.add(21, new Card("Seven", 7, "Hearts"));
-            deck.add(22, new Card("Seven", 7, "Clubs"));
-            deck.add(23, new Card("Seven", 7, "Diamonds"));
-
-            deck.add(24, new Card("Eight", 8, "Spades"));
-            deck.add(25, new Card("Eight", 8, "Hearts"));
-            deck.add(26, new Card("Eight", 8, "Clubs"));
-            deck.add(27, new Card("Eight", 8, "Diamonds"));
-
-            deck.add(28, new Card("Nine", 9, "Spades"));
-            deck.add(29, new Card("Nine", 9, "Hearts"));
-            deck.add(30, new Card("Nine", 9, "Clubs"));
-            deck.add(31, new Card("Nine", 9, "Diamonds"));
-
-            deck.add(32, new Card("Ten", 10, "Spades"));
-            deck.add(33, new Card("Ten", 10, "Hearts"));
-            deck.add(34, new Card("Ten", 10, "Clubs"));
-            deck.add(35, new Card("Ten", 10, "Diamonds"));
-
-            deck.add(36, new Card("Jack", 10, "Spades"));
-            deck.add(37, new Card("Jack", 10, "Hearts"));
-            deck.add(38, new Card("Jack", 10, "Clubs"));
-            deck.add(39, new Card("Jack", 10, "Diamonds"));
-
-            deck.add(40, new Card("Queen", 10, "Spades"));
-            deck.add(41, new Card("Queen", 10, "Hearts"));
-            deck.add(42, new Card("Queen", 10, "Clubs"));
-            deck.add(43, new Card("Queen", 10, "Diamonds"));
-
-            deck.add(44, new Card("King", 10, "Spades"));
-            deck.add(45, new Card("King", 10, "Hearts"));
-            deck.add(46, new Card("King", 10, "Clubs"));
-            deck.add(47, new Card("King", 10, "Diamonds"));
-
-            deck.add(48, new Card("Ace", 11, "Spades"));
-            deck.add(49, new Card("Ace", 11, "Hearts"));
-            deck.add(50, new Card("Ace", 11, "Clubs"));
-            deck.add(51, new Card("Ace", 11, "Diamonds"));
-        }
-
+        List<Card> deck = new ArrayList<>(52);
+        addCards(deck);
         shuffle(deck);
-
         return deck;
+    }
+
+    /**
+     * Add cards to the deck.
+     *
+     * @param deck the deck to which cards will be added
+     */
+    void addCards(List<Card> deck) {
+        String[] ranks = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+        String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
+
+        for (int i = 0; i < ranks.length; i++) {
+            for (String suit : suits) {
+                deck.add(new Card(ranks[i], values[i], suit));
+            }
+        }
     }
 
     /**
@@ -99,18 +49,16 @@ public class BlackJack {
      */
     public void shuffle(List<Card> deck) {
         Card temp;
-
         for (int i = 51; i >= 0; i--) {
             int randomIndex = (int) ((Math.random()) * (i + 1));
             temp = deck.get(i);
             deck.set(i, deck.get(randomIndex));
             deck.set(randomIndex, temp);
-
         }
     }
 
     /**
-     * Waiting correct input.
+     * Waiting for correct input.
      *
      * @return true if entered 1 and false if 0
      */
@@ -133,14 +81,12 @@ public class BlackJack {
         }
     }
 
-
     /**
      * Waiting n seconds.
      *
      * @param n number of seconds
      */
-    public void slp(int n) {
-
+    public void sleep(int n) {
         try {
             TimeUnit.SECONDS.sleep(n);
         } catch (InterruptedException e) {
@@ -149,157 +95,202 @@ public class BlackJack {
     }
 
     /**
+     * Starts a new round of the game.
+     *
+     * @param deck   the deck of cards
+     * @param user   the user
+     * @param dealer the dealer
+     * @return true if the game should continue, false otherwise
+     */
+    boolean playRound(List<Card> deck, User user, Dealer dealer) {
+        user.clear_hand(deck);
+        dealer.clear_hand(deck);
+        shuffle(deck);
+
+        user.take_card(deck, true);
+        user.take_card(deck, true);
+        dealer.take_card(deck, true);
+        dealer.take_card(deck, false);
+        dealer.points();
+
+        user.show_hand(true);
+        dealer.show_hand(false);
+
+        if (userTurn(user, dealer, deck)) return askForContinue();
+
+        dealerTurn(user, dealer, deck);
+
+        printScore(user, dealer);
+        return askForContinue();
+    }
+
+    /**
+     * Handles the user's turn.
+     *
+     * @param user   the user
+     * @param dealer the dealer
+     * @param deck   the deck of cards
+     * @return true if the round ends early, false otherwise
+     */
+    boolean userTurn(User user, Dealer dealer, List<Card> deck) {
+        while (true) {
+            user.points();
+            if (user.score == 21) {
+                System.out.println("You hit BlackJack. You win!!!");
+                sleep(1);
+                user.wins++;
+                return true;
+            }
+
+            if (user.score > 21) {
+                System.out.println("You have exceeded 21. You lose(");
+                sleep(1);
+                dealer.wins++;
+                return true;
+            }
+
+            System.out.println("Input a number");
+            sleep(1);
+
+            if (scan()) {
+                System.out.print("You take a card ");
+                user.take_card(deck, true).show(false);
+                sleep(1);
+                System.out.println();
+                user.show_hand(true);
+                dealer.show_hand(false);
+                sleep(1);
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Handles the dealer's turn.
+     *
+     * @param user   the user
+     * @param dealer the dealer
+     * @param deck   the deck of cards
+     */
+    void dealerTurn(User user, Dealer dealer, List<Card> deck) {
+        System.out.println("Dealer move\n------------");
+        sleep(1);
+        System.out.print("The dealer reveals a close card");
+        sleep(1);
+        dealer.hand.get(dealer.hand.size() - 1).open();
+        dealer.hand.get(dealer.hand.size() - 1).show(false);
+        System.out.println();
+
+        user.show_hand(true);
+        dealer.show_hand(true);
+
+        while (true) {
+            dealer.points();
+            if (dealer.score == 21) {
+                System.out.println("Dealer hit BlackJack. Dealer win!");
+                sleep(1);
+                dealer.wins++;
+                break;
+            }
+
+            if (dealer.score > 21) {
+                System.out.println("Dealer has exceeded 21. Dealer lose");
+                sleep(1);
+                user.wins++;
+                break;
+            }
+
+            if (dealer.score >= 17) {
+                determineWinner(user, dealer);
+                break;
+            }
+
+            sleep(2);
+            System.out.print("Dealer takes a card ");
+            dealer.take_card(deck, true).show(false);
+            sleep(2);
+            System.out.println();
+            user.show_hand(true);
+            dealer.show_hand(true);
+            sleep(1);
+        }
+    }
+
+    /**
+     * Determines the winner based on the scores.
+     *
+     * @param user   the user
+     * @param dealer the dealer
+     */
+    void determineWinner(User user, Dealer dealer) {
+        System.out.print("Game over. ");
+        sleep(1);
+
+        if (dealer.score > user.score) {
+            System.out.println("You lose.");
+            sleep(1);
+            dealer.wins++;
+        } else if (dealer.score < user.score) {
+            System.out.println("You win.");
+            sleep(1);
+            user.wins++;
+        } else {
+            System.out.println("Dead heat.");
+            sleep(1);
+            dealer.wins++;
+            user.wins++;
+        }
+    }
+
+    /**
+     * Prints the current score.
+     *
+     * @param user   the user
+     * @param dealer the dealer
+     */
+    void printScore(User user, Dealer dealer) {
+        System.out.println("Score: " + user.wins + "-" + dealer.wins);
+        sleep(1);
+    }
+
+    /**
+     * Asks the player if they want to continue playing.
+     *
+     * @return true if the player wants to continue, false otherwise
+     */
+    boolean askForContinue() {
+        System.out.println("Enter 1 if you want to continue and 0 if not.");
+        sleep(1);
+        return scan();
+    }
+
+    /**
      * Main game.
      *
      * @param args - args
      */
     public static void main(String[] args) {
-
-        boolean end;
-        int input;
-        BlackJack main = new BlackJack();
-        List<Card> deck = main.prepare_deck();
+        BlackJack game = new BlackJack();
+        List<Card> deck = game.prepare_deck();
 
         User user = new User();
         Dealer dealer = new Dealer();
 
         System.out.println("Welcome to BlackJack");
-        main.slp(1);
+        game.sleep(1);
+
         int rounds = 1;
-        //Игра
         while (true) {
             System.out.println("Round " + (rounds));
-            main.slp(1);
+            game.sleep(1);
             rounds++;
-            end = false;
 
-            user.clear_hand(deck);
-            dealer.clear_hand(deck);
-            main.shuffle(deck);
-
-            user.take_card(deck, true);
-            user.take_card(deck, true);
-            dealer.take_card(deck, true);
-            dealer.take_card(deck, false);
-            dealer.points();
-
-            user.show_hand(true);
-            dealer.show_hand(false);
-
-            //Ход игрока
-            while (!end) {
-                user.points();
-                if (user.score == 21) {
-                    System.out.println("You hit BlackJack. You win!!!");
-                    main.slp(1);
-                    end = true;
-                    user.wins++;
-                    break;
-                }
-
-                if (user.score > 21) {
-                    System.out.println("You have exceeded 21. You lose(");
-                    main.slp(1);
-                    end = true;
-                    dealer.wins++;
-                    break;
-                }
-
-                System.out.println("Input a number");
-                main.slp(1);
-
-                if (main.scan()) {
-                    System.out.print("You take a card ");
-                    user.take_card(deck, true).show(false);
-                    main.slp(1);
-                    System.out.println();
-                    user.show_hand(true);
-                    dealer.show_hand(false);
-                    main.slp(1);
-                } else {
-                    break;
-                }
-            }
-
-            //ход дилера
-            if (!end) {
-                System.out.println("Dealer move\n------------");
-                main.slp(1);
-                System.out.print("The dealer reveals a close card");
-                main.slp(1);
-                dealer.hand.get(dealer.hand.size() - 1).open();
-                dealer.hand.get(dealer.hand.size() - 1).show(false);
-                System.out.println();
-
-                user.show_hand(true);
-                dealer.show_hand(true);
-            }
-
-            while (!end) {
-
-                dealer.points();
-                if (dealer.score == 21) {
-                    System.out.println("Dealer hit BlackJack. Dealer win!");
-                    main.slp(1);
-                    end = true;
-                    dealer.wins++;
-                    break;
-                }
-
-                if (dealer.score > 21) {
-                    System.out.println("Dealer have exceeded 21. Dealer lose");
-                    main.slp(1);
-                    end = true;
-                    user.wins++;
-                    break;
-                }
-
-                if (dealer.score >= 17) {
-                    System.out.print("Game over. ");
-                    main.slp(1);
-
-                    if (dealer.score > user.score) {
-                        System.out.println("You lose.");
-                        main.slp(1);
-                        dealer.wins++;
-                    }
-                    if (dealer.score < user.score) {
-                        System.out.println("You win.");
-                        main.slp(1);
-                        user.wins++;
-                    }
-                    if (dealer.score == user.score) {
-                        System.out.println("Dead heat.");
-                        main.slp(1);
-                        dealer.wins++;
-                        user.wins++;
-                    }
-                    break;
-                }
-
-
-                main.slp(2);
-                System.out.print("Dealer take a card ");
-
-                dealer.take_card(deck, true).show(false);
-                main.slp(2);
-                System.out.println();
-                user.show_hand(true);
-                dealer.show_hand(true);
-                main.slp(1);
-
-            }
-
-            System.out.println("Score: " + user.wins + "-" + dealer.wins);
-            main.slp(1);
-            System.out.println("Enter 1 if you want to continue and 0 if not.");
-            main.slp(1);
-
-            if (!main.scan()) {
+            if (!game.playRound(deck, user, dealer)) {
                 break;
             }
             System.out.println("==============================");
         }
-
     }
 }
--- Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
@@ -1,35 +1,103 @@
 package ru.nsu.rebrin;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * Test.
- */
 public class BlackJackTest {
 
     private BlackJack blackJack;
+    private List<Card> testDeck;
+    private ByteArrayOutputStream outContent;
+    private final PrintStream originalOut = System.out;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
         blackJack = new BlackJack();
+
+        testDeck = new ArrayList<>(52);
+        testDeck.add(new Card("Two", 2, "Spades"));
+        testDeck.add(new Card("Two", 2, "Hearts"));
+        testDeck.add(new Card("Two", 2, "Clubs"));
+        testDeck.add(new Card("Two", 2, "Diamonds"));
+        testDeck.add(new Card("Three", 3, "Spades"));
+        testDeck.add(new Card("Three", 3, "Hearts"));
+        testDeck.add(new Card("Three", 3, "Clubs"));
+        testDeck.add(new Card("Three", 3, "Diamonds"));
+        testDeck.add(new Card("Four", 4, "Spades"));
+        testDeck.add(new Card("Four", 4, "Hearts"));
+        testDeck.add(new Card("Four", 4, "Clubs"));
+        testDeck.add(new Card("Four", 4, "Diamonds"));
+        testDeck.add(new Card("Five", 5, "Spades"));
+        testDeck.add(new Card("Five", 5, "Hearts"));
+        testDeck.add(new Card("Five", 5, "Clubs"));
+        testDeck.add(new Card("Five", 5, "Diamonds"));
+        testDeck.add(new Card("Six", 6, "Spades"));
+        testDeck.add(new Card("Six", 6, "Hearts"));
+        testDeck.add(new Card("Six", 6, "Clubs"));
+        testDeck.add(new Card("Six", 6, "Diamonds"));
+        testDeck.add(new Card("Seven", 7, "Spades"));
+        testDeck.add(new Card("Seven", 7, "Hearts"));
+        testDeck.add(new Card("Seven", 7, "Clubs"));
+        testDeck.add(new Card("Seven", 7, "Diamonds"));
+        testDeck.add(new Card("Eight", 8, "Spades"));
+        testDeck.add(new Card("Eight", 8, "Hearts"));
+        testDeck.add(new Card("Eight", 8, "Clubs"));
+        testDeck.add(new Card("Eight", 8, "Diamonds"));
+        testDeck.add(new Card("Nine", 9, "Spades"));
+        testDeck.add(new Card("Nine", 9, "Hearts"));
+        testDeck.add(new Card("Nine", 9, "Clubs"));
+        testDeck.add(new Card("Nine", 9, "Diamonds"));
+        testDeck.add(new Card("Ten", 10, "Spades"));
+        testDeck.add(new Card("Ten", 10, "Hearts"));
+        testDeck.add(new Card("Ten", 10, "Clubs"));
+        testDeck.add(new Card("Ten", 10, "Diamonds"));
+        testDeck.add(new Card("Jack", 10, "Spades"));
+        testDeck.add(new Card("Jack", 10, "Hearts"));
+        testDeck.add(new Card("Jack", 10, "Clubs"));
+        testDeck.add(new Card("Jack", 10, "Diamonds"));
+        testDeck.add(new Card("Queen", 10, "Spades"));
+        testDeck.add(new Card("Queen", 10, "Hearts"));
+        testDeck.add(new Card("Queen", 10, "Clubs"));
+        testDeck.add(new Card("Queen", 10, "Diamonds"));
+        testDeck.add(new Card("King", 10, "Spades"));
+        testDeck.add(new Card("King", 10, "Hearts"));
+        testDeck.add(new Card("King", 10, "Clubs"));
+        testDeck.add(new Card("King", 10, "Diamonds"));
+        testDeck.add(new Card("Ace", 11, "Spades"));
+        testDeck.add(new Card("Ace", 11, "Hearts"));
+        testDeck.add(new Card("Ace", 11, "Clubs"));
+        testDeck.add(new Card("Ace", 11, "Diamonds"));
     }
 
     @Test
-    void testPrepareDeck() {
+    void testprepare_deck() {
         List<Card> deck = blackJack.prepare_deck();
         assertNotNull(deck);
         assertEquals(52, deck.size());
-        assertTrue(deck.contains(new Card("Ace", 11, "Spades")));
-        assertTrue(deck.contains(new Card("King", 10, "Hearts")));
+        assertTrue(deck.containsAll(testDeck));
+    }
+
+    @Test
+    void testShuffle() {
+        List<Card> originalDeck = blackJack.prepare_deck();
+        blackJack.shuffle(originalDeck);
+        assertNotEquals(testDeck, originalDeck);
     }
 
     @Test
@@ -50,80 +118,36 @@ public class BlackJackTest {
     }
 
     @Test
-    public void test() {
-        BlackJack main = new BlackJack();
-        List<Card> deck = main.prepare_deck();
+    void testUserPointsCalculation() {
+        User user = new User();
+        user.take_card(testDeck, true);
+        user.take_card(testDeck, true);
+        user.points();
 
-        List<Card> deckTest = new ArrayList<Card>(52);
-        {
-            deckTest.add(0, new Card("Two", 2, "Spades"));
-            deckTest.add(1, new Card("Two", 2, "Hearts"));
-            deckTest.add(2, new Card("Two", 2, "Clubs"));
-            deckTest.add(3, new Card("Two", 2, "Diamonds"));
-
-            deckTest.add(4, new Card("Three", 3, "Spades"));
-            deckTest.add(5, new Card("Three", 3, "Hearts"));
-            deckTest.add(6, new Card("Three", 3, "Clubs"));
-            deckTest.add(7, new Card("Three", 3, "Diamonds"));
-
-            deckTest.add(8, new Card("Four", 4, "Spades"));
-            deckTest.add(9, new Card("Four", 4, "Hearts"));
-            deckTest.add(10, new Card("Four", 4, "Clubs"));
-            deckTest.add(11, new Card("Four", 4, "Diamonds"));
-
-            deckTest.add(12, new Card("Five", 5, "Spades"));
-            deckTest.add(13, new Card("Five", 5, "Hearts"));
-            deckTest.add(14, new Card("Five", 5, "Clubs"));
-            deckTest.add(15, new Card("Five", 5, "Diamonds"));
-
-            deckTest.add(16, new Card("Six", 6, "Spades"));
-            deckTest.add(17, new Card("Six", 6, "Hearts"));
-            deckTest.add(18, new Card("Six", 6, "Clubs"));
-            deckTest.add(19, new Card("Six", 6, "Diamonds"));
-
-            deckTest.add(20, new Card("Seven", 7, "Spades"));
-            deckTest.add(21, new Card("Seven", 7, "Hearts"));
-            deckTest.add(22, new Card("Seven", 7, "Clubs"));
-            deckTest.add(23, new Card("Seven", 7, "Diamonds"));
-
-            deckTest.add(24, new Card("Eight", 8, "Spades"));
-            deckTest.add(25, new Card("Eight", 8, "Hearts"));
-            deckTest.add(26, new Card("Eight", 8, "Clubs"));
-            deckTest.add(27, new Card("Eight", 8, "Diamonds"));
-
-            deckTest.add(28, new Card("Nine", 9, "Spades"));
-            deckTest.add(29, new Card("Nine", 9, "Hearts"));
-            deckTest.add(30, new Card("Nine", 9, "Clubs"));
-            deckTest.add(31, new Card("Nine", 9, "Diamonds"));
-
-            deckTest.add(32, new Card("Ten", 10, "Spades"));
-            deckTest.add(33, new Card("Ten", 10, "Hearts"));
-            deckTest.add(34, new Card("Ten", 10, "Clubs"));
-            deckTest.add(35, new Card("Ten", 10, "Diamonds"));
-
-            deckTest.add(36, new Card("Jack", 10, "Spades"));
-            deckTest.add(37, new Card("Jack", 10, "Hearts"));
-            deckTest.add(38, new Card("Jack", 10, "Clubs"));
-            deckTest.add(39, new Card("Jack", 10, "Diamonds"));
-
-            deckTest.add(40, new Card("Queen", 10, "Spades"));
-            deckTest.add(41, new Card("Queen", 10, "Hearts"));
-            deckTest.add(42, new Card("Queen", 10, "Clubs"));
-            deckTest.add(43, new Card("Queen", 10, "Diamonds"));
-
-            deckTest.add(44, new Card("King", 10, "Spades"));
-            deckTest.add(45, new Card("King", 10, "Hearts"));
-            deckTest.add(46, new Card("King", 10, "Clubs"));
-            deckTest.add(47, new Card("King", 10, "Diamonds"));
-
-            deckTest.add(48, new Card("Ace", 11, "Spades"));
-            deckTest.add(49, new Card("Ace", 11, "Hearts"));
-            deckTest.add(50, new Card("Ace", 11, "Clubs"));
-            deckTest.add(51, new Card("Ace", 11, "Diamonds"));
-        }
-
-        assertTrue(deck.containsAll(deckTest));
-
+        assertTrue(user.score > 0);
     }
 
+    @Test
+    void testDealerPointsCalculation() {
+        Dealer dealer = new Dealer();
+        dealer.take_card(testDeck, true);
+        dealer.take_card(testDeck, true);
+        dealer.points();
+
+        assertTrue(dealer.score > 0);
+    }
+
+    @Test
+    public void testMainGame() {
+
+        blackJack.in = new Scanner("0\n0\n");
+        InputStream originalIn = System.in;
+
+            User user = new User();
+            Dealer dealer = new Dealer();
+            blackJack.playRound(testDeck, user, dealer);
+
+            assertTrue(user.wins > 0 || dealer.wins > 0);
+
+    }
 }
