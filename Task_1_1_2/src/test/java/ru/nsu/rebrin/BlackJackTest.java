e2d63aeb4079858afa9bb91a466b8af08a5e41ff
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-27 14:42:30 +0700

    final

--- Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/BlackJackTest.java
@@ -12,7 +12,6 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
