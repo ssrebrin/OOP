5c3f5dac48d5170d8e73d52d33fbb331a811b958
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-09-27 18:28:43 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/rebrin/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/rebrin/CardTest.java
@@ -1,6 +1,5 @@
 package ru.nsu.rebrin;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
