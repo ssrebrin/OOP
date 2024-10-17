ea956eeaddfa677ba3776b7fa069a04a06879468
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-10-17 21:51:06 +0700

    a

--- Task_1_2_1/src/main/java/ru/nsu/rebrin/Graph.java
+++ Task_1_2_1/src/main/java/ru/nsu/rebrin/Graph.java
@@ -4,16 +4,79 @@ import java.io.IOException;
 import java.util.List;
 
 public interface Graph {
+    /**
+     * Add v.
+     */
     void addVertex();
+
+    /**
+     * Remove v.
+     * @param vertex - v
+     */
     void removeVertex(int vertex);
+
+    /**
+     * Add e.
+     * @param from - from
+     * @param to - to
+     */
     void addEdge(int from, int to);
+
+    /**
+     * Remove e.
+     * @param from - from
+     * @param to - to
+     */
     void removeEdge(int from, int to);
+
+    /**
+     * Get neighbors.
+     * @param vertex - v
+     * @return - neighbors
+     */
     List<Integer> getNeighbors(int vertex);
+
+    /**
+     * Read from file.
+     * @param filename - file name
+     * @throws IOException - Exception
+     */
     void readFromFile(String filename) throws IOException;
+
+    /**
+     * To string.
+     * @return - string
+     */
     String toString();
+
+    /**
+     * Equals.
+     * @param obj - obj
+     * @return - eq
+     */
     boolean equals(Object obj);
+
+    /**
+     * E count.
+     * @return - count
+     */
     int eCount();
+
+    /**
+     * V count.
+     * @return - count
+     */
     int vCount();
-    public List<Integer> topologicalSort();
+
+    /**
+     * Toposort.
+     * @return - List
+     */
+    List<Integer> topologicalSort();
+
+    /**
+     * Helper.
+     * @return - edges
+     */
     List<List<Integer>> getEdges();
 }
