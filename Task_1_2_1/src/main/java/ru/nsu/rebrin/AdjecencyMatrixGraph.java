3ea2599510ce41d33c1160428d546f919d57bf5f
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-10-18 13:51:40 +0700

    a

--- Task_1_2_1/src/main/java/ru/nsu/rebrin/AdjecencyMatrixGraph.java
+++ Task_1_2_1/src/main/java/ru/nsu/rebrin/AdjecencyMatrixGraph.java
@@ -3,7 +3,11 @@ package ru.nsu.rebrin;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Adj matrix.
--- Task_1_2_1/src/main/java/ru/nsu/rebrin/IncidenceMatrixGraph.java
+++ Task_1_2_1/src/main/java/ru/nsu/rebrin/IncidenceMatrixGraph.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
-import java.util.Objects;
+import java.util.Comparator;
 
 /**
  * Inc matrix.
