4084d97db250ed6feb9fda9aeabec2d8c68b706d
Sergei Rebrin <s.rebrin@g.nsu.ru>
2024-11-15 10:25:23 +0700

    a

--- Task_1_3_1/src/main/java/ru/nsu/rebrin/SubStr.java
+++ Task_1_3_1/src/main/java/ru/nsu/rebrin/SubStr.java
@@ -1,25 +1,30 @@
 package ru.nsu.rebrin;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * SubStr.
+ */
 public class SubStr {
 
     /**
-     * Find substring
+     * Find substring.
      *
-     * @param path - path
+     * @param path      - path
      * @param subString - ss
      * @return - array of index
      */
     static int[] sub_string(String path, String subString) {
 
-        if (subString.isEmpty()){
+        if (subString.isEmpty()) {
             return new int[0];
         }
 
-        int buff_size = 1000;
+        int buffSize = 1000;
         char[] buff = new char[1000];
         List<Integer> res = new ArrayList<>();
         Ss table = new Ss();
@@ -27,7 +32,7 @@ public class SubStr {
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             int i = 0;
 
-            br.read(buff, 0, buff_size);
+            br.read(buff, 0, buffSize);
 
             for (char a : buff) {
                 table.find(res, subString, a, i);
