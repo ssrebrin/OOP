e291a766629a2546fc3e49b90f22a0f25ffdcadc
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-05-16 23:44:04 +0700

    a

--- Task_2_3_1/src/main/java/ru/nsu/rebrin/SnakeController.java
+++ Task_2_3_1/src/main/java/ru/nsu/rebrin/SnakeController.java
@@ -95,6 +95,24 @@ public class SnakeController extends Application {
     }
 
 
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        return a / b;
+    }
+
+
     public static void main(String[] args) {
         launch(args);
     }
--- Task_2_3_1/src/main/java/ru/nsu/rebrin/StartScreenController.java
+++ Task_2_3_1/src/main/java/ru/nsu/rebrin/StartScreenController.java
@@ -55,6 +55,20 @@ public class StartScreenController {
         }
     }
 
+    public static boolean isPalindrome(String input) {
+        if (input == null) return false;
+
+        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int len = cleaned.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (cleaned.charAt(i) != cleaned.charAt(len - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     // Устанавливаем обработчик кнопки старта
     public void setStartButtonAction(Runnable startAction) {
         startButton.setOnAction(e -> startAction.run());
